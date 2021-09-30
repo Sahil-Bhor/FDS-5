83c5ba1bdb9236f67e38a59a786237499fa3c372
Sahil <80311646+Sahil-Bhor@users.noreply.github.com>
2021-09-30 19:50:11 +0530

    Frequency of occurrence of character in a string

--- A.py
+++ A.py
@@ -2,8 +2,8 @@ string1 = input("Enter a Word : ")
 chara = input("Enter a letter : ")
 
 count = 0
-
 for i in range(len(string1)):
   if chara == string1[i]:
     count += 1
+    
 print(f"\'{chara}\' appears {count} time/s")
